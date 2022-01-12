b11c855b822caa39a9cfc13734bf0c81451f4525
daol08 <daol08@naver.com>
2022-01-12 14:21:33 +0900

    파이어베이스.프로바이더..
    무언가 생겼습니다..

--- culture_app/lib/home.dart
+++ culture_app/lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
