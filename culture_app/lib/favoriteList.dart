c4885db2d3d0d5aef3414514d15b3e74452f267d
Dongwon Lee <ldw3097@gmail.com>
2022-01-13 14:38:40 +0900

    firestore를 찜목록이랑 연결(테스트용)

--- culture_app/lib/favoriteList.dart
+++ culture_app/lib/favoriteList.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:culture_app/contentCard.dart';
 import 'package:culture_app/provider/favoriteProvider.dart';
 import 'package:flutter/material.dart';
@@ -7,24 +8,45 @@ import 'bottomNav.dart';
 //Start App view
 class favoriteList extends StatelessWidget {
   favoriteList({Key? key}) : super(key: key);
+  CollectionReference contentCards =
+      FirebaseFirestore.instance.collection('contentCard');
 
+  //일단은 contentCard에 있는거 다 불러오게 했습니다
   @override
   Widget build(BuildContext context) {
-    var favorites = <contentCard>[];
-    favorites.add(
-        contentCard('realtitle', 'images/lake.jpg', 'place', '210101-220105'));
-    favorites.add(
-        contentCard('realtitle', 'images/lake.jpg', 'place', '210101-220105'));
+    return FutureBuilder<QuerySnapshot>(
+      future: contentCards.get(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          List<contentCard> cards = [];
+          var contents = snapshot.data!.docs.forEach((element) {
+            cards.add((contentCard(element['realtitle'], element['img'],
+                element['place'], element['period'])));
+          });
 
-    return Scaffold(
-        bottomNavigationBar: BottomNavigate(),
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('찜 목록'),
-        ),
-        body: ListView(
-          children: favorites,
-        ));
+          return Scaffold(
+              bottomNavigationBar: BottomNavigate(),
+              appBar: AppBar(
+                centerTitle: true,
+                title: const Text('찜 목록'),
+              ),
+              body: ListView(
+                children: cards,
+              ));
+        }
+
+        return Scaffold(
+            bottomNavigationBar: BottomNavigate(),
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('찜 목록'),
+            ),
+            body: CircularProgressIndicator());
+      },
+    );
   }
 }
 
